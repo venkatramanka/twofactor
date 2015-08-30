523cb003177805683e5d6133b506fad0f26da187
venkatramanka <venkatraman.anandakrishnan@listertechnologies.com>
2015-08-30 13:43:09 +0530

    Added config constants

--- lib/generators/twofactor/install_generator.rb
+++ lib/generators/twofactor/install_generator.rb
@@ -43,14 +43,16 @@ module Twofactor
 
       controller_name = options[:controller].present? ? options[:controller] : 'ApplicationController'
       table_name = options[:table_name].present? ? options[:table_name] : @model_name.tableize
+
       template 'templates/twofactor_config.rb', 'config/initializers/twofactor_config.rb'
+
       template 'templates/twofactor_migration.rb', File.join('db', 'migrate', "#{Time.now.strftime('%Y%m%d%H%M%S')}_add_twofactor_fields_to_#{table_name}.rb")
       template_location = controller_name == 'ApplicationController' ? 'twofactor' : controller_name.gsub('Controller','').tableize
       copy_file "templates/twofactor_register.html.#{options[:template_type]}", "app/views/#{template_location}/twofactor_register.html.#{options[:template_type]}"
       twofactor_actions = "\n  public\n"\
         "    def twofactor_register\n"\
-        "      model_object = #{@model_name.constantize}.find_by_id(params[:id])\n"\
-        "      qrcode = Twofactor.enable_twofactor_auth(model_object)\n"\
+        "      model_object = (Rails.application.class::TWOFACTOR_MODEL_NAME).constantize.find_by_id(params[:id])\n"\
+        "      qrcode = Twofactor::TwoStep.enable_twofactor_auth(model_object)\n"\
         "      render :template => \"#{template_location}/twofactor_register\", :locals => {:qrcode => qrcode}\n"\
         "    end\n\n"
       inject_into_file "app/controllers/#{controller_name.underscore}.rb", twofactor_actions, :before => /^end/
--- lib/twofactor.rb
+++ lib/twofactor.rb
@@ -1,26 +1,31 @@
 require 'base32'
 require 'totp'
+require 'rqrcode'
 module Twofactor
   class TwoStep
     def self.enable_twofactor_auth(model_object)
-      model_object.update_attribute('twofactor_secret', ::Base32.encode(rand(36**6).to_s(36)))
-      model_object.update_attribute('twofactor_enabled', true)
-      qrcode = ::RQRCode::QRCode.new("otpauth://totp/#{provider_name}:#{model_object[Rails.configuration.twofactor_reference_field]}?secret=#{model_object.twofactor_secret}&issuer=#{provider_name}", :size => 6, :level => :m)
+      model_object.update_attribute(Rails.application.class::TWOFACTOR_SECRET_ATTRIBUTE, ::Base32.encode(rand(36**6).to_s(36)))
+      model_object.update_attribute(Rails.application.class::TWOFACTOR_FLAG_ATTRIBUTE, true)
+      qrcode = ::RQRCode::QRCode.new("otpauth://totp/#{provider_name}:#{model_object[Rails.application.class::TWOFACTOR_REFERENCE_FIELD]}?secret=#{model_object[Rails.application.class::TWOFACTOR_SECRET_ATTRIBUTE]}&issuer=#{provider_name}", :size => Rails.application.class::TWOFACTOR_QRCODE_SIZE, :level => Rails.application.class::TWOFACTOR_QRCODE_QUALITY.to_sym)
       return qrcode
     end
     def self.disable_twofactor_auth(model_object)
-      model_object.update_attribute('twofactor_secret', nil)
-      model_object.update_attribute('twofactor_enabled', false)
+      model_object.update_attribute(Rails.application.class::TWOFACTOR_SECRET_ATTRIBUTE, nil)
+      model_object.update_attribute(Rails.application.class::TWOFACTOR_FLAG_ATTRIBUTE, false)
       return true
     end
     def self.valid_code?(model_object, code)
-      ::TOTP.valid?(model_object[Rails.configuration.twofactor_secret_attribute], code)
+      ::TOTP.valid?(model_object[Rails.application.class::TWOFACTOR_SECRET_ATTRIBUTE], code)
     end
 
     private
 
     def self.provider_name
-      Rails.application.config.session_options[:key].sub(/^_/,'').sub(/_session/,'')
+      if Rails.application.class::TWOFACTOR_PROVIDER_NAME.blank?
+        return Rails.application.config.session_options[:key].sub(/^_/,'').sub(/_session/,'')
+      else
+        return Rails.application.class::TWOFACTOR_PROVIDER_NAME
+      end
     end
   end
 end
