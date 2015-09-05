bf17b3008e3383a8f9d4dc0ee3291122f3a05c80
venkatramanka <venkatraman.anandakrishnan@listertechnologies.com>
2015-09-05 09:25:15 +0530

    0.0.2 release commit

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '11604ee2be4cc215d4a71129836ad6a495545523518eefa1e14a12d01720a52de1bb45d4de43a71b666ab1bc43f7f2f512eaffbe129048fb66a55d1ddb4bc2f9'
--- test/dummy/spec/twofactor_test.rb
+++
@@ -1,5 +0,0 @@
-RSpec.describe "First test" do
-  context 'dummy test' do
-    it { expect(1).to equal 1 }
-  end
-end
